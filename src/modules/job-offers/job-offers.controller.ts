180d1ab32de227125e0d843b9af96897e23e6ce6
lenoox <62141973+lenoox@users.noreply.github.com>
2023-01-04 11:57:12 +0100

    add methods to job offers

--- src/modules/job-offers/job-offers.controller.ts
+++ src/modules/job-offers/job-offers.controller.ts
@@ -1,16 +1,29 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Post} from '@nestjs/common';
 import { JobOffersService } from './job-offers.service';
+import {JobOfferEntity} from "./entities/job-offer.entity";
 
 @Controller('job-offers')
 export class JobOffersController {
+
   constructor(private jobOffersService: JobOffersService) {}
 
+  @Get(':id')
+  findOne(@Param('id') id: string): Promise<JobOfferEntity> {
+    return this.jobOffersService.findOne(id);
+  }
+
   @Get('/')
-  findAll(): any {
+  findAll(): Promise<JobOfferEntity[]> {
     return this.jobOffersService.findAll();
   }
+
   @Post('/')
-  create(@Body() jobOffers: any): any {
+  create(@Body() jobOffers: any): Promise<JobOfferEntity> {
     return this.jobOffersService.create(jobOffers);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.jobOffersService.remove(id);
   }
 }
--- src/modules/job-offers/job-offers.service.ts
+++ src/modules/job-offers/job-offers.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { JobOfferEntity } from './entities/job-offer.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import {JobOfferDto} from "./dto/job-offer.dto";
 
 @Injectable()
 export class JobOffersService {
@@ -9,10 +10,24 @@ export class JobOffersService {
     @InjectRepository(JobOfferEntity)
     private jobOfferRepository: Repository<JobOfferEntity>,
   ) {}
-  create(jobOffer) {
+
+  create(jobOffer: JobOfferDto) {
     return this.jobOfferRepository.save(jobOffer);
   }
+
   findAll() {
     return this.jobOfferRepository.find();
+  }
+
+  findOne(id: string) {
+    return this.jobOfferRepository.findOneBy({ id });
+  }
+
+  update(id: string, jobOfferDto: JobOfferDto) {
+    return this.jobOfferRepository.update(id, jobOfferDto);
+  }
+
+  remove(id: string) {
+    return this.jobOfferRepository.delete(id);
   }
 }
