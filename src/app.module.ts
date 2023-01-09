458b02628c3ee4d7030d5897530a772e2b4420f3
lenoox <62141973+lenoox@users.noreply.github.com>
2023-01-09 10:06:19 +0100

    Revert "Feature/add methods to job offers (#9)" (#10)
    This reverts commit f2f8baa17c4fc454c1c1990626f9efc909817339.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,32 +1,9 @@
 import { Module } from '@nestjs/common';
-import {ConfigModule, ConfigService} from "@nestjs/config";
-import {TypeOrmModule} from "@nestjs/typeorm";
-import {JobOffersModule} from "./modules/job-offers/job-offers.module";
-import {AppService} from "./app.service";
-import {AppController} from "./app.controller";
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule.forRoot()],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('POSTGRES_HOST'),
-        port: configService.get('POSTGRES_PORT'),
-        username: configService.get('POSTGRES_USER'),
-        password: configService.get('POSTGRES_PASSWORD'),
-        database: configService.get('POSTGRES_DB'),
-        entities: ["dist/src/**/*.entity{ .ts,.js}"],
-        migrations: ["dist/migrations/*{.ts,.js}"],
-        synchronize: true,
-        autoLoadEntities: true,
-        migrationsRun: false
-      }),
-    }),
-    JobOffersModule,
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'HR-Talent';
+    return 'Hello World!';
   }
 }
