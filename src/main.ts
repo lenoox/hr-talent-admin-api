c196db4bf68af418dd028cabc5d74b401a1c9eee
lenoox <62141973+lenoox@users.noreply.github.com>
2023-04-30 23:26:56 +0200

    add ssl

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import * as cookieParser from 'cookie-parser';
-
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('./certs/key.pem'),
+    cert: fs.readFileSync('./certs/cert.pem'),
+  };
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
   const configService = app.get(ConfigService);
   const frontendUrls = configService.get<string>('FRONTEND_URLS');
   app.enableCors({
