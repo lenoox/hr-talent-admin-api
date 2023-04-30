26126df2e81bb65b90f27cc1af698b65c950e576
lenoox <62141973+lenoox@users.noreply.github.com>
2023-05-01 00:29:21 +0200

    add ssl

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import * as cookieParser from 'cookie-parser';
 import * as fs from 'fs';
-const keyFile = fs.readFileSync(__dirname + '/../certs/key.pem');
-const certFile = fs.readFileSync(__dirname + '/../certs/cert.pem');
+const keyFile = fs.readFileSync(__dirname + '/../../certs/key.pem');
+const certFile = fs.readFileSync(__dirname + '/../../certs/cert.pem');
 async function bootstrap() {
   const httpsOptions = {
     key: keyFile,
