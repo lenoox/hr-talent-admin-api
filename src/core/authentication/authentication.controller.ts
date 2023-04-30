9c93d46ce555e0dc2669f5073fe7df29ec594187
lenoox <62141973+lenoox@users.noreply.github.com>
2023-04-30 21:52:02 +0200

    add cookie

--- src/core/authentication/authentication.controller.ts
+++ src/core/authentication/authentication.controller.ts
@@ -5,7 +5,10 @@ import {
   HttpCode,
   Post,
   UseGuards,
-  Get, ClassSerializerInterceptor, UseInterceptors,
+  Get,
+  ClassSerializerInterceptor,
+  UseInterceptors,
+  Res,
 } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import { RegisterDto } from './dto/register.dto';
@@ -14,13 +17,15 @@ import { LocalAuthenticationGuard } from './local-authentication.guard';
 import { JwtAuthenticationGuard } from './jwt-authentication.guard';
 import { UsersService } from '../../modules/users/users.service';
 import { JwtRefreshGuard } from './jwt-refresh.guard';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('authentication')
 @UseInterceptors(ClassSerializerInterceptor)
 export class AuthenticationController {
   constructor(
     private readonly authenticationService: AuthenticationService,
-    private readonly usersService: UsersService
+    private readonly usersService: UsersService,
+    private readonly configService: ConfigService,
   ) {}
 
   @Post('register')
@@ -31,23 +36,39 @@ export class AuthenticationController {
   @HttpCode(200)
   @UseGuards(LocalAuthenticationGuard)
   @Post('log-in')
-  async logIn(@Req() request: RequestWithUser) {
+  async logIn(
+    @Req() request: RequestWithUser,
+    @Res({ passthrough: true }) res,
+  ) {
+    const frontendUrls = this.configService.get<string>('FRONTEND_URLS');
     const { user } = request;
-    const accessTokenCookie = this.authenticationService.getCookieWithJwtAccessToken(user.id);
-    const {
-      cookie: refreshTokenCookie,
-      token: refreshToken
-    } = this.authenticationService.getCookieWithJwtRefreshToken(user.id);
+    const accessTokenCookie =
+      this.authenticationService.getCookieWithJwtAccessToken(user.id);
+    const { cookie: refreshTokenCookie, token: refreshToken } =
+      this.authenticationService.getCookieWithJwtRefreshToken(user.id);
 
     await this.usersService.setCurrentRefreshToken(refreshToken, user.id);
-
-    request.res.setHeader('Set-Cookie', [accessTokenCookie, refreshTokenCookie]);
+    //
+    // request.res.setHeader('Set-Cookie', [
+    //   accessTokenCookie,
+    //   refreshTokenCookie,
+    // ]);
+    res.cookie('Authentication', accessTokenCookie, {
+      httpOnly: true,
+      sameSite: 'none',
+      secure: 'false',
+    });
+    res.cookie('Authentication', refreshTokenCookie, {
+      httpOnly: true,
+      sameSite: 'none',
+      secure: 'false',
+    });
 
     if (user.isTwoFactorAuthenticationEnabled) {
       return;
     }
 
-    return user;
+    return res;
   }
 
   @UseGuards(JwtAuthenticationGuard)
@@ -55,7 +76,10 @@ export class AuthenticationController {
   @HttpCode(200)
   async logOut(@Req() request: RequestWithUser) {
     await this.usersService.removeRefreshToken(request.user.id);
-    request.res.setHeader('Set-Cookie', this.authenticationService.getCookiesForLogOut());
+    request.res.setHeader(
+      'Set-Cookie',
+      this.authenticationService.getCookiesForLogOut(),
+    );
   }
 
   @UseGuards(JwtAuthenticationGuard)
@@ -67,7 +91,8 @@ export class AuthenticationController {
   @UseGuards(JwtRefreshGuard)
   @Get('refresh')
   refresh(@Req() request: RequestWithUser) {
-    const accessTokenCookie = this.authenticationService.getCookieWithJwtAccessToken(request.user.id);
+    const accessTokenCookie =
+      this.authenticationService.getCookieWithJwtAccessToken(request.user.id);
 
     request.res.setHeader('Set-Cookie', accessTokenCookie);
     return request.user;
