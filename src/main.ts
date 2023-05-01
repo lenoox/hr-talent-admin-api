a3e2bc98c05efd69a1f9d12779ef0b3fc1b26b61
lenoox <62141973+lenoox@users.noreply.github.com>
2023-05-01 13:28:19 +0200

    add ssl

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import * as cookieParser from 'cookie-parser';
 import * as fs from 'fs';
-import * as path from 'path';
+import * as express from 'express';
+import * as http from 'http';
+import * as https from 'https';
+import { ExpressAdapter } from '@nestjs/platform-express';
 async function bootstrap() {
   try {
     const keyPath = process.env.SSL_KEY_PATH || '';
@@ -14,9 +17,8 @@ async function bootstrap() {
     };
     console.log(`Load key path ${keyPath}`);
     console.log(`Load cert path ${certPath}`);
-    const app = await NestFactory.create(AppModule, {
-      httpsOptions,
-    });
+    const server = express();
+    const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
     const configService = app.get(ConfigService);
     const frontendUrls = configService.get<string>('FRONTEND_URLS');
     app.enableCors({
@@ -24,9 +26,9 @@ async function bootstrap() {
       origin: frontendUrls.split(';'),
     });
     app.use(cookieParser());
-    await app.listen(process.env.PORT).then(() => {
-      console.log(`Start application on ${process.env.PORT} port`);
-    });
+    await app.init();
+    http.createServer(server).listen(3000);
+    https.createServer(httpsOptions, server).listen(443);
   } catch (error) {
     console.error(error);
   }
