620419775d5be59e2f19a3a1e6a8cdf9c86f114c
lenoox <62141973+lenoox@users.noreply.github.com>
2023-05-01 12:30:15 +0200

    add ssl

--- src/main.ts
+++ src/main.ts
@@ -5,22 +5,26 @@ import * as cookieParser from 'cookie-parser';
 import * as fs from 'fs';
 import * as path from 'path';
 async function bootstrap() {
-  const keyPath = process.env.SSL_KEY_PATH || '';
-  const certPath = process.env.SSL_CERT_PATH || '';
-  const httpsOptions = {
-    key: fs.readFileSync(path.join(__dirname, keyPath)),
-    cert: fs.readFileSync(path.join(__dirname, certPath)),
-  };
-  const app = await NestFactory.create(AppModule, {
-    httpsOptions,
-  });
-  const configService = app.get(ConfigService);
-  const frontendUrls = configService.get<string>('FRONTEND_URLS');
-  app.enableCors({
-    credentials: true,
-    origin: frontendUrls.split(';'),
-  });
-  app.use(cookieParser());
-  await app.listen(8443);
+  try {
+    const keyPath = process.env.SSL_KEY_PATH || '';
+    const certPath = process.env.SSL_CERT_PATH || '';
+    const httpsOptions = {
+      key: fs.readFileSync(keyPath, 'utf8'),
+      cert: fs.readFileSync(certPath, 'utf8'),
+    };
+    const app = await NestFactory.create(AppModule, {
+      httpsOptions,
+    });
+    const configService = app.get(ConfigService);
+    const frontendUrls = configService.get<string>('FRONTEND_URLS');
+    app.enableCors({
+      credentials: true,
+      origin: frontendUrls.split(';'),
+    });
+    app.use(cookieParser());
+    await app.listen(process.env.PORT);
+  } catch (error) {
+    console.error(error);
+  }
 }
 bootstrap();
